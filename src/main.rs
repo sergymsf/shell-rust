00979eb62f9e0d128799c4d9dbb3bcdf3d3ebb1d
sergio <sergio@LAPTOP-GBA5NQSH>
2024-06-02 23:49:14 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
-    
-    let stdin = io::stdin();
-    let mut input = String::new();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        
-        stdin.read_line(&mut input).unwrap();
-        input.pop();
-        println!("{}: command not found", input);
-        input.clear();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        if command.is_empty() {
+            continue;
+        }
+        if command == "exit 0" {
+            process::exit(0);
+        }
+
+        println!("{}: command not found", command);
     }
 }
