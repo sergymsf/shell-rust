9591c3570d9349d75865c0e34997b0d08541e58d
sergio <sergio@LAPTOP-GBA5NQSH>
2024-06-03 11:38:20 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
+
+    let builtins = ["echo", "exit", "type"];
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -19,6 +22,16 @@ fn main() {
         }
         if command.starts_with("echo ") {
             println!("{}", &command[5..]);
+            continue;
+        }
+
+        if command.starts_with("type ") {
+            let to_check = &command[5..];
+            if builtins.contains(&to_check) {
+                println!("{} is a shell builtin", to_check);
+            } else {
+                println!("{} not found", to_check);
+            }
             continue;
         }
 
